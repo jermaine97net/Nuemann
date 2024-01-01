effb3f036e5f010590539d97966f49e66d33cd19
jermaine97net <155339545+jermaine97net@users.noreply.github.com>
2024-01-01 17:43:30 +0000

    commit

--- Python/Test/forecast_test.py
+++ Python/Test/forecast_test.py
@@ -3,37 +3,39 @@ import yfinance as fin
 import numpy as np
 run_read = 0
 
-# if run_read:
-#     file_name = 'data.txt'
-#     with open(file_name, 'r') as file:
-#         data = [float(line.strip()) for line in file.read().split()]
+if run_read:
+    file_name = 'data.txt'
+    with open(file_name, 'r') as file:
+        data = [float(line.strip()) for line in file.read().split()]
 
-# data = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+first_val=0
+events=4
+num_forecats=3
 
-# events=2
-# num_forecats=3
+pos_prob = 0.6
+neg_prob = 1 - pos_prob
 
-# pos_prob = 0.6
-# neg_prob = 1 - pos_prob
+prob = [neg_prob, pos_prob]
 
-# prob = [neg_prob, pos_prob]
+pos_distr = [0.5, 1.5]
+neg_distr = [-1.5, -0.5]
 
-# pos_distr = [0.5, 1.5]
-# neg_distr = [-1.5, -0.5]
+def rand():
+  return random.uniform(*pos_distr) if random.random()>= random.uniform(*prob) else random.uniform(*neg_distr)
 
-# def rand():
-#   return random.uniform(*pos_distr) if random.random()>= random.uniform(*prob) else random.uniform(*neg_distr)
-
-result=[]
-sum_arr=[]
-events=10
-
+arr = []
+val_2=[]
+first_val=0
 for i in range(events):
-    result.append(sum_arr.append(i))
-    
-result_2=[[1+i for i in range(i+1)] for i in range(events)]
+    arr.append(rand())
+    val_2.append(first_val+sum(arr[:i+1]))
+    print(f'## arr {arr}')
+    print(val_2)
+# print(f'val_2 {val_2}')
+# print(forecasts)
 
 # print(sum_arr)
-print(result)
-print(result_2)
+# print(result)
+# print(result_2)
 # print(len(forecasts))

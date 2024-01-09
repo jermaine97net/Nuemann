229ef5103c2df4b09055d852031171c73076d059
jermaine97net <155339545+jermaine97net@users.noreply.github.com>
2024-01-09 15:30:15 +0000

    commit

--- Files/Script_files/test_run.py
+++ Files/Script_files/test_run.py
@@ -1,7 +1,26 @@
 import pandas as pd
 import random
 import numpy as np
+import itertools
+import random
 
-data = (5,6,3,4)
-a = np.char(':')
-print(data[a])
+number = 28
+data = np.array(range(10))
+indices = np.concatenate(np.where((data > 5) & (data < 9)))
+print(indices)
+indices = itertools.filterfalse(lambda x: x + number > len(data) - 1, indices)
+print(list(indices))
+
+
+# for val in indices:
+#     if val == len(data) - 1:  # Check if val is the last index
+#         continue
+#     print((data[val],data[val+number]))
+
+# data_2  = ((1,2),(3,4),(5,6),(7,8))
+
+# mapped = itertools.starmap(lambda x,y: x + y, data_2)
+# # map_2 = map(lambda x,y: x+y, data_2)
+
+# print(list(mapped))
+# # print(list(map_2))
